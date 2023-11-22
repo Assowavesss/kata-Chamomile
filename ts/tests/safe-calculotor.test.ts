77305d950570af37639322e0ee673b67200c1e6c
popolito1 <paulineblanc92@gmail.com>
2023-11-22 10:32:13 +0100

    test: test safe-calculotor
    Co-authored-by: Bastien <bastien707@users.noreply.github.com>
    Co-authored-by: Tenkoh <BlanchardNicolas@users.noreply.github.com>
    Co-authored-by: Neith <SwevenNeith@users.noreply.github.com>
    Co-authored-by: Jonathan <Assowavesss@users.noreply.github.com>

--- ts/tests/safe-calculotor.test.ts
+++ ts/tests/safe-calculotor.test.ts
@@ -1,6 +1,18 @@
-import { test } from "@jest/globals"
+import { test, expect } from "@jest/globals"
+import { SafeCalculator, Authorizer } from "../safe-calculator"
+
+
+class AuthorizerImpl implements Authorizer {
+  authorize(): boolean {
+      return true;
+  }
+} 
+
 
 test('should not throw when authorized', () => {
+  
   // TODO: write a test that fails due to the bug in
   // SafeCalculator.add()
+  const safeCalc = new SafeCalculator(new AuthorizerImpl);
+  expect(safeCalc.add(1,2)).toBe(3);
 })
